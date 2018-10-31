336185b125664ea142d97f93003ca99cc55b0c66
Paul Tonning <paul.tonning@student.vvc.edu>
2018-10-30 17:23:20 -0700

    update

--- chapter05-classes/examples/email/main.cpp
+++ chapter05-classes/examples/email/main.cpp
@@ -2,9 +2,9 @@
 #include "ccc_time.h"
 #include "message.h"
 
-
 int main()
 {
+
     Message m("Paul Tonning","John Doe",Time());
     m.append("This is the first line of the message");
     m.append("This is the second line of the message");
--- chapter05-classes/examples/email/message.cpp
+++ chapter05-classes/examples/email/message.cpp
@@ -1,1 +1,9 @@
 #include "message.h"
+
+
+Message::Message(std::string rec,std::string send,Time t)
+{
+    m_recipient = rec;
+    m_sender = send;
+    m_timestamp = t;
+}
