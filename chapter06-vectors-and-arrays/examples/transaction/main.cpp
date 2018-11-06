f1ac361a70a4de7012a870e6c771a01c3977ed05
Paul Tonning <paul.tonning@student.vvc.edu>
2018-11-06 12:40:36 -0800

    update

---
+++ chapter06-vectors-and-arrays/examples/transaction/main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include "transaction.h"
+
+// NOTE!!!  put Transactionlog in a separate file 
+class Transactionlog
+{
+    std::vector<Transaction> m_transactions;
+    double m_balance;
+    double m_min_balance;
+    double m_average_balance;
+
+public:
+    void add_transaction(const Transaction &t);
+    int min_daily_balance();
+    int average_daily_balance();
+    void print();
+};
+
+
+int main()
+{
+    Transactionlog tlog;
+
+    tlog.add_transaction(Transaction(15,-255,"Cash Withdraw"));
+    
+    tlog.print();
+
+    return 0;
+}
